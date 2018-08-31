e2fcf799a0bb77a78dc78e26b5fa7561c5703ea2
Vincent Santos <vsantos2@mail.sfsu.edu>
2018-08-31 16:38:23 -0700

    Unreal Project Setup

---
+++ ue_buildingescape/Source/UE_BuildingEscape/UE_BuildingEscapeGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE_BuildingEscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_BUILDINGESCAPE_API AUE_BuildingEscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
